e39ba2e401a6649b29520c501f11f004e5a93cda
OwenMt50 <Omartin005@student.butte.edu>
2025-05-21 03:11:33 +0000

    commit

--- Project#4.cpp
+++ Project#4.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace std; //Below are all functions that do the math within int main, less cluttered code
 
-int first(int pounds, double stone){ //Function for Part #1
+float first(int pounds, double stone){ //Function for Part #1
     return pounds*stone;
 }
 
@@ -13,7 +13,7 @@ int second(int feet, float centimeter){ //Function for Part #2
     return feet*centimeter;
 }
 
-int third(int football, float meters){ //Function for Part #3
+float third(int football, float meters){ //Function for Part #3
     return football*meters;
 }
 
@@ -30,11 +30,17 @@ int main (){
 
     cout<<"Part #1 'How many stones now!?'\n"; //Part #1 intro
     cout<<"You left your weight scale on pounds to stones, and the scale refuses to revert itself to the proper measurements.\n";
-    cout<<"Input the amount of pounds you weigh, to get your weight in stones.\n";
+    cout<<"Input the number of pounds you weigh to get your weight in stones.\n";
     cin>>pounds; //User input
-    cout<<"You weigh about "<<first(pounds, stone)<<" stones.\n"; //Displays what user weighs in stones
+    cout<<"\n";
 
-    cout<<"____________________________________________________________________________________________________________________________________________________________\n";
+    if(pounds <= 0){
+        cout<<"You stepped onto the scale, but it's stuck at zero...I wonder what caused this?";
+    }else{
+        cout<<"You weigh about "<<first(pounds, stone)<<" stones.\n"; //Displays what user weighs in stones
+    }
+
+    cout<<"________________________________________________________________________________\n";
     cout<<"\n"; //First line to separate Part #1 from Part #2
 
     int feet = 0;
@@ -45,9 +51,15 @@ int main (){
     cout<<"'That seems quite easy.'You said to yourself, but oddly enough you must input how tall you are in feet to have it be converted into centimeters.\n";
     cout<<"Input how tall you are in feet:\n";
     cin>>feet; //User input
-    cout<<"You are "<<feet<<" feet tall, and measure roughly "<<second(feet, centimeter)<<" centimeters.\n"; //Displays how tall user is in feet and centimeters
+    cout<<"\n";
 
-    cout<<"____________________________________________________________________________________________________________________________________________________________\n";
+    if(feet <= 0){
+        cout<<"You entered zero on the application, this surely won't affect your odds...";
+    }else{
+        cout<<"You are "<<feet<<" feet tall, and measure roughly "<<second(feet, centimeter)<<" centimeters.\n"; //Displays how tall user is in feet and centimeters
+    }
+
+    cout<<"________________________________________________________________________________\n";
     cout<<"\n"; //Second line to separate Part #2 from Part #3
 
     int football = 0;
@@ -58,15 +70,25 @@ int main (){
     cout<<"You have been tasked to run as many american football field lengths as you can, which is 100 yards for this scenario.\n";
     cout<<"Once you entered the amount of football fields you have ran, it will be converted into meters.\n";
     cin>>football; //User input 
+    cout<<"\n";
 
     if(football <=0){ //If statement, if user decides to enter 0 then they opted to skip the run (not exactly intended more incase if user decides to enter 0)
         cout<<"You decided to opt out of this task.\n";
     }else{
         cout<<"You ran "<<third(football, meters)<<" meters, impressive but do you think you can do better?\n";
     }
+    
+    cout<<"________________________________________________________________________________\n";
+    cout<<"\n"; //Separating Part #3 from results
 
     cout<<"|RESULTS|\n"; //Start of results output, displays all previous parts into 1 whole sentence
-    cout<<"All in all you weigh "<<first(pounds, stone)<<" stones, "<<second(feet, centimeter)<<" centimeters tall, and can run "<<third(football, meters)<<" meters.\n"; 
+    if(pounds <= 0, feet <= 0, football <=0){
+        cout<<"It appears that you weigh "<<first(pounds, stone)<<" stones, "<<second(feet, centimeter)<<" centimeters tall, and ran "<<third(football, meters)<<" meters!?.\n";
+        cout<<"Hmm, you entered zero for everything didn't you?\n";
+    }else{
+        cout<<"All in all you weigh "<<first(pounds, stone)<<" stones, "<<second(feet, centimeter)<<" centimeters tall, and can run "<<third(football, meters)<<" meters.\n"; 
+    }
+
     cout<<"\n"; //space for separating last question
     cout<<"To end this program input 1, or to restart press 2.\n";
     cin>>No;
